f3e4e35f61ae6f3ba9e7146a2cbf6ff9ab2d0edc
randyjhc <soc.randyjhc@gmail.com>
2015-10-04 16:01:30 +0800

    fix t_ff connections

--- ripple_carry_counter.v
+++ ripple_carry_counter.v
@@ -13,8 +13,8 @@ input           clk;
 input           rst;
 
 t_ff tff0(q[0], clk, rst);
-t_ff tff1(q[1], clk, rst);
-t_ff tff2(q[2], clk, rst);
-t_ff tff3(q[3], clk, rst);
+t_ff tff1(q[1], q[0], rst);
+t_ff tff2(q[2], q[1], rst);
+t_ff tff3(q[3], q[2], rst);
 
 endmodule
